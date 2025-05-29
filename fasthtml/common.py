74d330c2239712704906f8fbd15efa62ed32678f
Micah Jamison <dataconcise@gmail.com>
2025-05-29 09:36:17 -0700

    remove requirements for fastlite and uvicorn for toga android

--- fasthtml/common.py
+++ fasthtml/common.py
@@ -1,12 +1,12 @@
-import uvicorn
+# import uvicorn
 from dataclasses import dataclass
 from typing import Any
 
 from .starlette import *
 from fastcore.utils import *
 from fastcore.xml import *
-from apswutils import Database
-from fastlite import *
+# from apswutils import Database
+# from fastlite import *
 from .basics import *
 from .pico import *
 from .authmw import *
